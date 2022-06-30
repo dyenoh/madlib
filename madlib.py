9d5b030bbdd2afda4577bd7b1081b84cd8cdd6b5
dyenoh <108141631+dyenoh@users.noreply.github.com>
2022-06-30 15:28:52 +0300

    first commit

---
+++ madlib.py
@@ -0,0 +1,14 @@
+#youtuber = "dyeno"
+
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribeto {youtuber}")
+
+
+adj1 = input("adjective: ")
+verb1 = input("verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"I like money because it makes me {adj1}. If money wasn't a problem i would {verb1} and be like {famous_person} "
+
+print(madlib)
